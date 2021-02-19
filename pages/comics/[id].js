413ac9106291434d1207119cfabac49dff558d36
prullansky <owenchboy@gmail.com>
2021-02-19 14:23:34 +0100

    added some more comics

--- pages/comics/[id].js
+++ pages/comics/[id].js
@@ -1,8 +1,8 @@
-import React from "react";
-import styles from "../../styles/Home.module.css";
-import Navbar from "../navbar";
-import { comicsList } from "./data.js";
-import { useRouter } from "next/router";
+import React from 'react'
+import styles from '../../styles/Home.module.css'
+import Navbar from '../navbar'
+import { comicsList } from './data.js'
+import { useRouter } from 'next/router'
 
 // This function is only called on the server.
 //
@@ -14,59 +14,61 @@ import { useRouter } from "next/router";
 // It's weird, but that's how Next API works. Not sure why
 // they made that decision
 export async function getServerSideProps(context) {
-  return {
-    props: {
-      comicId: context.query.id
+    return {
+        props: {
+            comicId: context.query.id,
+        },
     }
-  }
 }
 
 export default function ComicPage(props) {
-  const router = useRouter();
+    const router = useRouter()
 
-  // here if router.query.id is empty, we use the `comicId`
-  // from props that was populated by `getServerSideProps`
-  // above
-  const comicId = router.query.id ?? props.comicId;
+    // here if router.query.id is empty, we use the `comicId`
+    // from props that was populated by `getServerSideProps`
+    // above
+    const comicId = router.query.id ?? props.comicId
 
-  const currentComic = comicsList.find((comic) => comic.id === comicId);
+    const currentComic = comicsList.find((comic) => comic.id === comicId)
 
-  console.log("ID", comicId);
-  // console.log('DATA', data)
-  console.log("currentComic", currentComic);
+    console.log('ID', comicId)
+    // console.log('DATA', data)
+    console.log('currentComic', currentComic)
 
-  if (!currentComic) {
-    return (
-      <div>
-        <h1 className={styles.title}>ale rodriguez</h1>
-        <div className={styles.layoutComics}>
-          <Navbar />
-          <h2>Sorry, comic not found</h2>
-        </div>
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h1 className={styles.title}>ale rodriguez</h1>
-
-      <div className={styles.layoutComics}>
-        <Navbar />
-
-        <h2>{currentComic.description}</h2>
-
-        {currentComic.images.map((image, imageIndexInArray) => {
-          return (
-            <div
-              key={`${comicsId}-${imageIndexInArray}`}
-              className={styles.containerComics}
-            >
-              <img src={image} />
+    if (!currentComic) {
+        return (
+            <div>
+                <h1 className={styles.title}>ale rodriguez</h1>
+                <div className={styles.layoutComics}>
+                    <Navbar />
+                    <h2>Sorry, comic not found</h2>
+                </div>
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+        )
+    }
+
+    return (
+        <div>
+            <h1 className={styles.title}>ale rodriguez</h1>
+
+            <div className={styles.layoutComics}>
+                <Navbar />
+                {/* <h2>{currentComic.title}</h2>
+                <ul>
+                    <li>{currentComic.description}</li>
+                </ul> */}
+
+                {currentComic.images.map((image, imageIndexInArray) => {
+                    return (
+                        <div
+                            key={`${comicId}-${imageIndexInArray}`}
+                            className={styles.containerComics}
+                        >
+                            <img src={image} className={styles.comic} />
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
