93a91b5f4415cc5e2f8171eae6214e37cc113237
prullansky <owenchboy@gmail.com>
2021-03-18 14:46:45 +0100

    Implemented functional responsive Navbar with dropdown menu / fixing small details

--- components/Navbar2.js
+++ components/Navbar2.js
@@ -7,7 +7,12 @@ import { useState } from 'react'
 export default function Navbar2() {
   const router = useRouter()
 
-  const [showLinks, setShowLinks] = useState(false)
+  const [showLinks, setShowLinks] = useState(true)
+
+  const displayHidden = (e) => {
+    setShowLinks(!showLinks)
+    console.log(showLinks)
+  }
 
   return (
     <div className={styles.navbar}>
@@ -17,26 +22,48 @@ export default function Navbar2() {
             <a>Home</a>
           </Link>
         </div>
-        <div className={styles.hidden}>
-          <Link href="/web_projects">
-            <a>Web Projects</a>
-          </Link>
+        <div className={showLinks ? styles.hidden : styles.show}>
+          <div className={router.pathname == '/' ? styles.current : ''}>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </div>
 
-          <Link href="/illustration">
-            <a>Illustration</a>
-          </Link>
-          <Link href="/comics">
-            <a>Comics</a>
-          </Link>
-          <Link href="/video">
-            <a>Video</a>
-          </Link>
+          <div
+            className={router.pathname == '/web_projects' ? styles.current : ''}
+          >
+            <Link href="/web_projects">
+              <a>Web Projects</a>
+            </Link>
+          </div>
 
-          <Link href="/bio">
-            <a>Bio</a>
-          </Link>
+          <div
+            className={router.pathname == '/illustration' ? styles.current : ''}
+          >
+            <Link href="/illustration">
+              <a>Illustration</a>
+            </Link>
+          </div>
+
+          <div className={router.pathname == '/comics' ? styles.current : ''}>
+            <Link href="/comics">
+              <a>Comics</a>
+            </Link>
+          </div>
+
+          <div className={router.pathname == '/video' ? styles.current : ''}>
+            <Link href="/video">
+              <a>Video</a>
+            </Link>
+          </div>
+
+          <div className={router.pathname == '/bio' ? styles.current : ''}>
+            <Link href="/bio">
+              <a>Bio</a>
+            </Link>
+          </div>
         </div>
-        <button>Open</button>
+        <button onClick={displayHidden}>Open</button>
       </div>
 
       <div className={styles.rightSide}>
