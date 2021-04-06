e70a8c53bca49b9b3b6f2462f70e7a4c50b2e41e
prullansky <owenchboy@gmail.com>
2021-04-06 19:47:46 +0200

    added new project in Web Projects

--- data/WebProjectsData.js
+++ data/WebProjectsData.js
@@ -3,7 +3,7 @@ export const WebProjectsData = [
     id: 'strive_together',
     year: 2020,
     team: ['Katie Gracey', 'Jan Völker', 'Ale Rodriguez'],
-    title: 'Strive Together',
+    title: 'Strive Together - Prototype',
     picture: '/web_projects/strive_together.png',
     environment:
       'HTML5 | CSS3 | JavaScript | NodeJs | ExpressJS | React JS | MongoDB',
@@ -16,7 +16,7 @@ export const WebProjectsData = [
     id: 'gamsters',
     year: 2020,
     team: ['Jan Völker', 'Ale Rodriguez'],
-    title: 'Gamsters: pick a game already!',
+    title: 'Gamsters: pick a game already! - Prototype',
     picture: '/web_projects/gamsters.png',
     environment:
       'HTML5 | CSS3 | JavaScript | NodeJs | ExpressJS | Axios | Handlebars | Atlas API | MongoDB',
@@ -24,12 +24,29 @@ export const WebProjectsData = [
       'Board-game random generator that helps you choosing what to play, by filtering through an API.',
     github: 'https://github.com/Gamsters/gamsters',
     demo: 'http://gamster.herokuapp.com/',
+    responsabilities: [
+      'Stablished and developed the main idea, while leading a team of three people',
+      'Built the main skeleton of the app',
+      'Implemented the filtering logic',
+    ],
+  },
+  {
+    id: 'portfolio',
+    year: 2020,
+    team: ['Ale Rodriguez'],
+    title: 'portfolio',
+    picture: '/web_projects/illustration.png',
+    environment: 'HTML5 | CSS3 | JavaScript | NextJs',
+    description:
+      'Portfolio for my illustrations and comics. Built with Next.js for learning purposes. (Under construction)',
+    github: 'https://github.com/prullansky/illustration_portfolio',
+    demo: 'https://illustration-portfolio-prullansky.vercel.app',
   },
   {
     id: 'shapeZ',
     year: 2020,
     team: ['Ale Rodriguez'],
-    title: 'shapeZ',
+    title: 'shapeZ - Prototype',
     picture: '/web_projects/shapez.jpg',
     environment: 'HTML5 | CSS3 | JavaScript | Canvas',
     description:
